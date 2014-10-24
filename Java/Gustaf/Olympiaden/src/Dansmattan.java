021f5fdb1ab362b9ed102bbcb97a59b9421dd531
Gustaf Rosenblad <gustaf@rosenblad.ws>
2014-10-24 15:25:15 +0200

    Created project and added test code

---
+++ Java/Gustaf/Olympiaden/src/Dansmattan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Dansmattan {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String s = scanner.next();
+		System.out.println("Text: " + s);
+		
+		scanner.close();
+	}
+}
