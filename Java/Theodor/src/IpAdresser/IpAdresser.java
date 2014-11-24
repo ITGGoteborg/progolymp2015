0b8b739dae80713c655f33e341a9f609e8ca9ff8
Theodor Angergard <portalprogramming@gmail.com>
2014-11-24 21:34:33 +0100

    Started working on IPAdresser

---
+++ Java/Theodor/src/IpAdresser/IpAdresser.java
@@ -0,0 +1,24 @@
+package IpAdresser;
+
+import java.util.Scanner;
+
+public class IpAdresser {
+
+	public IpAdresser(){
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Sekvens ? ");
+		String adress = scanner.next();
+		
+		while(1 < 2){
+			
+		}
+		
+		scanner.close();
+	}
+	
+	public static void main(String[] args){
+		new IpAdresser();
+	}
+	
+}
