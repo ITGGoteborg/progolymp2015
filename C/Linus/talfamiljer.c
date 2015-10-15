4f6a92a5c10ccdcc87d0ef76e693cb4322db3b28
Linus Styrén <Linkaan@users.noreply.github.com>
2015-10-15 21:07:59 +0200

    Nu ska programmet köras ännu snabbare.

--- C/Linus/talfamiljer.c
+++ C/Linus/talfamiljer.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#define MAX 1000000
 
 int main() {
-  int N,i,j,k,p[10],n[100];
-  int mask[1000000];
+  int N,p[10],n[100];
+  int mask[MAX] = {0};
+  register int i, j, k;
 
   scanf("%d", &N);
   for(i=0;i<N;i++) scanf("%d", &n[i]);
-  memset(mask,0,sizeof(mask));
-  for(i=1;i<1000000;i++) {
+  i = MAX+1;
+  while(--i) {
     k=i;
-    for(j=0;j<10;j++) p[j]=0;
+    p[0]=0;
+    p[1]=0;
+    p[2]=0;
+    p[3]=0;
+    p[4]=0;
+    p[5]=0;
+    p[6]=0;
+    p[7]=0;
+    p[8]=0;
+    p[9]=0;
     while (k>0) {p[k%10]++; k/=10;}
 
     for(j=1;j<10;j++) if(p[j]>0) {k=j; p[j]--; break;}
     for(j=0;j<10;j++) while (p[j]>0) {k=10*k+j; p[j]--;}
     for(j=0;j<N;j++) if(i%n[j]==0) mask[k]|=(1<<j);
   }
-  for(i=1;i<1000000;i++) if(mask[i]==(1<<N)-1) break;
+  for(i=1;i<MAX;i++) if(mask[i]==(1<<N)-1) break;
   fprintf(stdout, "%d\n", i);
 }
