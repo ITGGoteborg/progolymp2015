84363162cba46fe0bf0ab65d2de145a9f985d0cf
Linus Styrén <linus122xbb@gmail.com>
2015-10-16 09:03:45 +0200

    Programmet klarar alla testfiler på 0,16s som är 0,03s långsammare än det snabbaste!

--- C/Linus/talfamiljer.c
+++ C/Linus/talfamiljer.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#define MAX 1000000
 
 int main() {
-  int N,i,j,k,p[10],n[100];
-  int mask[1000000];
+  int N,n[100];
+  int p[10] = {0};
+  int mask[MAX] = {0};
+  register int i, j, k;
 
   scanf("%d", &N);
   for(i=0;i<N;i++) scanf("%d", &n[i]);
-  for(i=1;i<1000000;i++) mask[i]=0;
-  for(i=1;i<1000000;i++) {
+  i = MAX+1;
+  while(--i) {
+    int maskBits = 0;
+    for(j=0;j<N;j++) {
+        if(i%n[j]==0)
+            maskBits |= (1 << j);
+    }
+    if (maskBits == 0)
+        continue;
     k=i;
-    for(j=0;j<10;j++) p[j]=0;
     while (k>0) {p[k%10]++; k/=10;}
 
     for(j=1;j<10;j++) if(p[j]>0) {k=j; p[j]--; break;}
     for(j=0;j<10;j++) while (p[j]>0) {k=10*k+j; p[j]--;}
-    for(j=0;j<N;j++) if(i%n[j]==0) mask[k]|=(1<<j);
+    mask[k] |= maskBits;
   }
-  for(i=1;i<1000000;i++) if(mask[i]==(1<<N)-1) break;
+  for(i=1;i<MAX;i++) if(mask[i]==(1<<N)-1) break;
   fprintf(stdout, "%d\n", i);
 }
